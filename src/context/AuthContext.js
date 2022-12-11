14ceb008c7c4edf56c13e7cb26db48d140724cd8
pgermano <philipgermano@gmail.com>
2022-12-11 16:55:34 -0500

    basic state for auth0

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -4,9 +4,18 @@ import SInfo from "react-native-sensitive-info";
 import Auth0 from "react-native-auth0";
 import jwtDecode from "jwt-decode";
 
+
+
 const auth0 = new Auth0({
   domain: AUTH0_DOMAIN,
   clientId: AUTH0_CLIENT_ID,
 });
+
+const AuthContextProvider = (props) => {
+    const [loading, setLoading] = useState(true);
+    const [loggedIn, setLoggedIn] = useState(null);
+    const [userData, setUserData] = useState(null);
+
+}
 
 const AuthContext = React.createContext();
